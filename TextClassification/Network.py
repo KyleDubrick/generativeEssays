a3692088879c08692a7350f613c539f526d51f4a
kyled <kyle.dubrick@live.com>
2017-09-22 21:57:28 -0400

    Added functions in network.py

--- TextClassification/Network.py
+++ TextClassification/Network.py
@@ -1,2 +1,11 @@
 import numpy as np
 import tensorflow as tf
+
+#Functions to produce weights and bias variables, can change std deviation if we want
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=1)
+    return tf.Variable(initial)
+
+def bias_variable(shape):
+  initial = tf.constant(0.1, shape=shape)
+  return tf.Variable(initial)
